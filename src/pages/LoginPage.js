c55c9e36b849b76d996fd0382fdea60f004cdc3d
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-10-02 13:41:47 -0300

    configurando o firebase e testanto login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, TextInput, StyleSheet, Button } from 'react-native';
+import firebase from '@firebase/app';
+import '@firebase/auth';
 
 import FormRow from '../components/FormRow'
 
@@ -11,6 +13,34 @@ export default class LoginPage extends React.Component {
             mail: '',
             password: '',
         }
+    }
+
+    componentDidMount() {
+        // Your web app's Firebase configuration
+        const firebaseConfig = {
+            apiKey: "AIzaSyB0GJuEIAA9QIvFfggtr_3xHMuqEQUSgxw",
+            authDomain: "series-9ce76.firebaseapp.com",
+            databaseURL: "https://series-9ce76.firebaseio.com",
+            projectId: "series-9ce76",
+            storageBucket: "series-9ce76.appspot.com",
+            messagingSenderId: "187647241475",
+            appId: "1:187647241475:web:e590c2965a68cbf78d8d36"
+        };
+        // Initialize Firebase
+        if(firebase.apps.length === 0) {
+            firebase.initializeApp(firebaseConfig);
+        }
+
+        firebase.
+            auth()
+            .signInWithEmailAndPassword('teste@mail.com', '123123')
+            .then(user => {
+                console.log('Usuário autenticado!', user);
+            })
+            .catch(error => {
+                console.log('Usuário NÂO encontrado', error);
+            }
+            );
     }
 
     onCHangeHandler(field, value) {
